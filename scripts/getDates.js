12779aa57ae0bc01b7e969ab42c77e24e3933d7b
camellecastro <camelleccastro@gmail.com>
2024-01-19 00:39:48 -0500

    Chamber Web Structure

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -4,4 +4,4 @@ const year = todaysDate.getFullYear();
 document.getElementById('year').textContent = year;
 
 const lastUpdate = document.lastModified;
-document.querySelector('#lastModified').textContent = `Last Modification: ${lastUpdate}`
+document.querySelector('#lastModified').textContent = `Last Modification: ${lastUpdate}`;
