d055913b9026dd85429cb6cf505a192fdf20013b
camellecastro <camelleccastro@gmail.com>
2024-01-26 01:46:35 -0500

    home page enhancement

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -5,3 +5,18 @@ document.getElementById('year').textContent = year;
 
 const lastUpdate = document.lastModified;
 document.querySelector('#lastModified').textContent = `Last Modification: ${lastUpdate}`;
+
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
+
